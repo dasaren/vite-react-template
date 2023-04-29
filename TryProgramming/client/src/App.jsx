d315176c69875beec25ca02c963c806ea419d012
dasare <96350966+dasaren@users.noreply.github.com>
2023-04-29 15:47:24 +0000

    update

--- TryProgramming/client/src/App.jsx
+++ TryProgramming/client/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import { AuthProvider } from "app/context/AuthContext";
+import { AuthProvider } from "app/context/AuthContext";
+import LoginD from "components/LoginD";
+import RegisterPage from "components/RegisterPage";
+import SignUp from "components/Register";
+import SignIn from "components/SignIn";
+import PersonalLogin from "components/PersonalLogin";
+import SignOut from "components/SignOut";
+import Home from "pages/Home";
+import PageNotFound from "pages/PageNotFound";
+import ProfileScreen from "components/ProfileScreen";
+import ChatHomeScreen from "features/chatDS/screens/ChatHomeScreen";
+import GetTopicFeed from "features/chatDS/screens/GetTopicFeed";
+
+// import "vite/modulepreload-polyfill";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+  const helmetContext = {};
+
+  const [message, setMessage] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="">
+      <HelmetProvider context={helmetContext}>
+        <QueryClientProvider client={client}>
+          <ToastContainer />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route>
+                  <Route path="/login" element={<LoginD />} />
+                  <Route path="/" element={<ChatHomeScreen />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                  <Route path="/registerme" element={<SignUp />} />
+                  <Route path="/signin" element={<SignIn />} />
+                  <Route path="/psignin" element={<PersonalLogin />} />
+                  <Route path="/signout" element={<SignOut />} />
+                  <Route path="*" element={<PageNotFound />} />
+
+                  <Route path="/profile" element={<ProfileScreen />} />
+
+                  <Route path="/">
+                    <Route index element={<ChatHomeScreen />} />
+                    <Route path=":slug?" element={<GetTopicFeed />} />
+                  </Route>
+                </Route>
+              </Routes>
+              {/* </Layout> */}
+            </AuthProvider>
+          </Router>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- TryProgramming/client/vite.config.js
+++ TryProgramming/client/vite.config.js
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+import djangoVite from 'django-vite-plugin'
+import react from '@vitejs/plugin-react-swc'
+import jsconfigPaths from 'vite-jsconfig-paths'
+import { resolve } from 'path'
+import path from 'path'
+
+
+
+
+
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths(), 
+    // djangoVite(
+    //   {
+    //     input: {
+    //       main: resolve(__dirname, 'index.html')
+    //       // maini: resolve(__dirname, 'dist'),
+    //       // main: resolve(__dirname, 'dist/index.html'),
+    //   }
+    // }
+    // )
+//     djangoVite([
+//     'home/js/app.js',
+//     'home/css/style.css',
+// ])
+],
+  resolve: {
+    alias: {
+       "~": path.resolve(__dirname, "node_modules"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    manifest: true ,
+    chunkSizeWarningLimit: 16000,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+  base: process.env.mode === "production" ? "/static/" : "/",
+  // root: "./src"
 })
