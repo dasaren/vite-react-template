aee987bc6079bdce6df4d2c46bb10cdcff459eee
dasare <96350966+dasaren@users.noreply.github.com>
2023-05-08 13:32:00 +0000

    sure it will be wrong

--- vite.config.js
+++ vite.config.js
@@ -27,43 +27,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react(), jsconfigPaths(),
+export default defineConfig({
+  plugins: [react(), jsconfigPaths(),
   
   
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// })
-
-
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
-
-  return {
-    plugins: [
-      react(),
-      jsconfigPaths(),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    define: {
-      // Pass the environment variables to the client-side code
-      'process.env': process.env,
-    },
-    build: {
-      rollupOptions: {
-        plugins: [
-          // Use environment variables for production build
-          isProduction && require('dotenv').config({ path: '.env.production' }),
-        ].filter(Boolean),
-      },
-    },
-  };
-});
+  },
+})
