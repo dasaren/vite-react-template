83f0df057fce6bf6b198ea0e6ca316ba40d41e37
dasare <96350966+dasaren@users.noreply.github.com>
2023-05-09 17:06:21 +0000

    3rd apps

--- src/features/chatDS/components/DsApps.jsx
+++ src/features/chatDS/components/DsApps.jsx
@@ -1,6 +1,6 @@
 import Layout from "components/Layout";
 import React from "react";
-import ecommerceImage from "assets/ecommerce.png";
+// import ecommerceImage from "assets/ecommerce.png";
 import { Link } from "react-router-dom";
 
 const DsApps = ({ app_list }) => {
