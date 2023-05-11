cc7b0a0af5723e666497610e647738b2dc243983
dasare <96350966+dasaren@users.noreply.github.com>
2023-05-11 12:03:36 +0000

    added slice to app

--- src/app/rootReducer.jsx
+++ src/app/rootReducer.jsx
@@ -1,10 +1,15 @@
 import { combineReducers } from "redux";
 import myUserReducer from "../features/redux-users/myUserSlice";
 import chatReducer from "features/chatDS/slice/chatSlice";
+import commerceReducer from "features/commerce/Slice/commerceSlice";
+import commerceCartReducer from "features/commerce/Slice/cartSlice";
 
 const rootReducer = combineReducers({
   myuser: myUserReducer,
   chat: chatReducer,
+  commerce: commerceReducer,
+  cart: commerceCartReducer,
+  
 });
 
 export default rootReducer;
